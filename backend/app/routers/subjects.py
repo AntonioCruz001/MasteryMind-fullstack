384e6bf6be816495a5991589653aacf60e9473af
AntonioCruz001 <antonio_avcb@hotmail.com>
2026-06-24 14:18:03 -0300

    Estrutura finalizada: Auth, MySQL, Relacionamento User-Subject

--- backend/app/routers/subjects.py
+++ backend/app/routers/subjects.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 
-from app import crud, schemas
+from app import crud, schemas, models
+from app.dependencies import get_current_user
 from app.database import SessionLocal
+
 
 router = APIRouter(
     prefix="/subjects",
@@ -20,11 +22,15 @@ def get_db():
 
 # Rota para criar um assunto
 @router.post("/", response_model=schemas.SubjectRead)
-def create_subject(subject: schemas.SubjectCreate, db: Session = Depends(get_db)):
-    return crud.create_subject(db = db, subject = subject)
+def create_subject(subject: schemas.SubjectCreate, db: Session = Depends(get_db),
+                   current_user : models.User = Depends(get_current_user)):
+    
+    return crud.create_subject(db = db, subject = subject, user_id = current_user.id)
 
 # Rota para Listar os assuntos
 @router.get("/", response_model=List[schemas.SubjectRead])
-def read_subjects(skip: int = 0, limit: int = 100, db:Session = Depends(get_db)):
-    subjects = crud.get_subjects(db, skip = skip, limit = limit)
+def read_subjects(skip: int = 0, limit: int = 100, db:Session = Depends(get_db),
+                  current_user : models.User = Depends(get_current_user)):
+    
+    subjects = crud.get_subjects(db, skip = skip, limit = limit, user_id = current_user.id)
     return subjects
