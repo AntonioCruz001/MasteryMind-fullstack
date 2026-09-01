565c4a808c9b5415a726de1e539764d757c9a617
AntonioCruz001 <antonio_avcb@hotmail.com>
2026-09-01 16:59:11 -0300

    Nova lógica de buscar os cards no backend

--- frontend/src/components/FlashcardItem.jsx
+++ frontend/src/components/FlashcardItem.jsx
@@ -1,29 +1,75 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import Button from "./Button";
 import FlashcardContent from "./FlashcardContent";
 import ReviewControls from "./ReviewControls";
 
 export default function FlashcardItem() {
+    const { subjectId } = useParams()
     const [fliped, setFliped] = useState(true);
     const [frontContent, setFrontContent] = useState('');
     const [backContent, setBackContent] = useState('');
-    
+    const [cardContent, setCardContent] = useState({ front: "", back: "" });
 
-    function handleFlipClick (){
-        {fliped === false ? setFliped(true) : setFliped(false)}
+    const [pontos,setPontos] = useState(0)
+
+    const [flashcardsArray, setFlashcardsArray] = useState([])
+    const [reviewedCards, setReviewedCards] = useState({})
+
+    function handleFlipClick() {
+        { fliped === false ? setFliped(true) : setFliped(false) }
     }
+
+    useEffect(() => {
+        const fetchFlashcards = async () => {
+            try {
+                const response = await api.get(`/subjects/${subjectId}/flashcards`);
+                const cards = response.data;
+
+                const initialReviewedState = {};
+                cards.forEach(card => {
+                    if (card.is_reviwed) {
+                        initialReviewedState[card.id] = true;
+                    }
+                });
+
+                setReviewedCards(initialReviewedState);
+                setFlashcardsArray(cards);
+            } catch (err) {
+                console.log('Erro ao buscar flashcards:', err);
+            }
+        };
+        fetchFlashcards()
+    }, [subjectId])
+
+    // Ordenar - Revisado para o final
+    const sortedFlashcards = useMemo(() => {
+        return [...flashcards].sort((a, b) => {
+            const aReviewed = reviewedCards[a.id] ? 1 : 0;
+            const bReviewed = reviewedCards[b.id] ? 1 : 0;
+
+            return aReviewed - bReviewed; // Não revisados (0) primeiro, revisados (1) ao final
+        });
+    }, [flashcardsArray, reviewedCards]);
+
+
+
+
+
+
+
 
     return (
         // wrapper
         <div className="rounded-2xl bg-stone-300 shadow-sm flex flex-col gap-4 p-4">
             {/* header */}
             <div className="flex flex-row  px-4">
-                <ReviewControls />
+                <ReviewControls cardPontos={pontos} />
                 <Button className="w-6" title={'⚙'} />
             </div>
 
             {/* frente e verso - conteudo */}
-            <FlashcardContent onClick={handleFlipClick} front={frontContent} back={backContent}/>
+            <FlashcardContent onClick={handleFlipClick} front={frontContent} back={backContent} />
 
             {/* Botoes */}
             <div>
--- frontend/src/components/ReviewControls.jsx
+++ frontend/src/components/ReviewControls.jsx
@@ -1,19 +1,40 @@
 import { useState } from "react"
-export default function ReviewControls() {
-    let statusColors = ['X', 'o', 'o', 'o']
-    let actualStatusColor = "border-2 border-pink-800"
-    let offStatusColor = ""
+export default function ReviewControls({cardPontos}) {
+    // const accentColor = "border-2 border-pink-800"
+    const accentColor = "border-2 border-b-indigo-700"
+
+    // let cardPontos = pontos     // Preciso dos pontos do card
+
+    const [statusColors, setStatusColors] = useState([
+        { actualStatusColor: "bg-gray-500", offStatusColor: "bg-gray-200" },
+        { actualStatusColor: "bg-red-500", offStatusColor: "bg-red-200" },
+        { actualStatusColor: "bg-orange-500", offStatusColor: "bg-orange-200" },
+        { actualStatusColor: "bg-lime-500", offStatusColor: "bg-lime-200" },
+        { actualStatusColor: "bg-green-500", offStatusColor: "bg-green-200" },
+    ])
+
     // busca dos dados do card no backend
+    // Map() a partir do index dos pontos - OK
 
     return <div className="flex flex-row justify-between px-4 grow">
+        
         <div>data de revisao</div>
-        {/* <div>{statusColors[actualStatusColor]}</div> */}
-        <div className="flex flex-row items-center">
-            <div className={`rounded-full bg-red-300 h-4 w-4 ${actualStatusColor} `}></div>
-            <div className={`rounded-full bg-orange-300 h-4 w-4 ${offStatusColor} `}></div>
-            <div className={`rounded-full bg-lime-300 h-4 w-4 ${offStatusColor} `}></div>
-            <div className={`rounded-full bg-green-500 h-4 w-4 ${offStatusColor} `}></div>
-        </div>
-    </div>
 
+        <ul className="flex flex-row items-center gap-2 ">
+            {statusColors.map((color, index) => {
+                // Se cardPontos for 5, 6... o Math.min trava o valor em 4 (5ª bolinha)
+                const activeIndex = Math.min(cardPontos, statusColors.length - 1);
+
+                return (
+                    <li
+                        key={index}
+                        className={
+                            `rounded-full h-4 w-4 ${activeIndex === index ?
+                                `${color.actualStatusColor} ${accentColor}` : color.offStatusColor}`
+                        }
+                    ></li>
+                );
+            })}
+        </ul>
+    </div>
 }
